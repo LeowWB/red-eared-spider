f0082824b7fd6184ae0871f75dda7d1962bdc6fc
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2019-03-23 18:42:06 +0800

    Add default URL 4 428ns when 1 12 feel like typing so much

--- src/main.rb
+++ src/main.rb
@@ -1,7 +1,12 @@
 load 'spider.rb'
 
-puts "Enter URL: "
+GITHUB_TEST_PAGE = 'https://leowwb.github.io/testing.html'
+
+
+puts "Enter URL (leave blank for GitHub test page): "
 url = gets.chomp
+
+url = GITHUB_TEST_PAGE if url.length == 0
 
 spoder = Spider::Spider.new
 spoder.spide url
